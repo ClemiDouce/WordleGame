484483ed35dd0468cd68d46e9287fe9c01dcc857
clem <dupycournau@gmail.com>
2022-01-12 00:06:48 +0100

    update search

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # Init variables
 secret_word = ""
 user_input = ""
-searched_letters = ""
+found_letters = set()
+searched_letters = set()
+
 try_count = 0
 max_try = 3
 
 # Choose a word
-secret_word = "uburluesque"
+secret_word = "partir"
 
 # Ask an input
 while len(user_input) != len(secret_word):
@@ -19,11 +21,16 @@ while try_count < max_try:
     for index, char in enumerate(user_input):
         if char == temp_char[index]:
             result += "🟩"
+            found_letters.add(char)
+            # if char in searched_letters:
+            # searched_letters.remove(char)
         elif temp_char.count(char) > 0:
             result += "🟨"
             temp_char = temp_char.replace(char, '.', 1)
+            # searched_letters.add(char)
         else:
             result += "⬜"
+            searched_letters.add(char)
     print("Result : ", result)
 
     if user_input == secret_word:
@@ -31,6 +38,6 @@ while try_count < max_try:
         break
     else:
         try_count += 1
+        # print("Lettres trouvées : ", ', '.join(found_letters))
+        # print("Lettres cherchées : ", ', '.join(searched_letters))
         user_input = input(f"Enter a word ({len(secret_word)} letters) : ")
-
-# Send color for each letter 🟩🟨⬜
