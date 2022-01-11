12307dcaced9428fd611c672f232e91e8932b3dd
clem <dupycournau@gmail.com>
2022-01-11 23:15:39 +0100

    input asked

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello")
+# Init variables
+secret_word = ""
+user_input = ""
+searched_letters = ""
+
+# Choose a word
+secret_word = "fantome"
+
+# Ask an input
+user_input = input(f"Enter a word ({len(secret_word)} letters")
+
+# Check the input with word
+print(secret_word, user_input)
+
+# Send color for each letter 🟩🟨⬜
